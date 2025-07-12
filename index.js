c2c7b7fb1fd7a71b66c26b7da2476a6794133da7
Akhileshyadu00 <akhileshyadu001@gmail.com>
2025-07-12 07:42:37 +0530

    test1

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 4001;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://you-tube-1-iota.vercel.app'],
+  origin: ['http://localhost:5173', 'https://youtubeclone1-silk.vercel.app'],
   credentials: true
 } 
 ))
